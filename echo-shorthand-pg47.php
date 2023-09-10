1d2261bdad06663f73b657c11e56c52ee5bd4b7a
spesmeaindeo <90931037+spesmeaindeo@users.noreply.github.com>
2023-09-10 13:15:39 -0700

    Pg 47 added favorite candy to array for  list

--- echo-shorthand-pg47.php
+++ echo-shorthand-pg47.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Jenn';
+$favorites = ['Reese Peices','Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
